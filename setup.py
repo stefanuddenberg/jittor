781a0bbee8857b01908e2688e847037a8e48fe06
Stefan Uddenberg <rahonavis75@gmail.com>
2020-09-09 23:34:24 -0400

    force installation on elementary os

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import os
+from setuptools import setup, find_packages
+import setuptools
 error_msg = "Jittor only supports Ubuntu>=16.04 currently."
 
 try:
@@ -9,11 +12,11 @@ try:
             m[a[0]] = a[1].replace("\"", "")
 except:
     raise RuntimeError(error_msg)
-assert m["NAME"] == "Ubuntu" and float(m["VERSION_ID"])>16, error_msg
 
-import setuptools
-from setuptools import setup, find_packages
-import os
+m["NAME"] = "Ubuntu"
+m["VERSION_ID"] = 18
+assert m["NAME"] == "Ubuntu" and float(m["VERSION_ID"]) > 16, error_msg
+
 
 path = os.path.dirname(__file__)
 with open(os.path.join(path, "README.md"), "r", encoding='utf8') as fh:
@@ -28,7 +31,7 @@ with open(os.path.join(path, "python/jittor/__init__.py"), "r", encoding='utf8')
         raise RuntimeError("Unable to find version string.")
 
 setuptools.setup(
-    name='jittor',  
+    name='jittor',
     version=version,
     # scripts=[],
     author="Jittor Group",
@@ -40,9 +43,11 @@ setuptools.setup(
     # packages=setuptools.find_packages(),
     python_requires='>=3.7',
 
-    packages=["jittor", "jittor.test", "jittor.models", "jittor.utils", "jittor_utils"],
+    packages=["jittor", "jittor.test", "jittor.models",
+              "jittor.utils", "jittor_utils"],
     package_dir={'': os.path.join(path, 'python')},
-    package_data={'': ['*', '*/*', '*/*/*','*/*/*/*','*/*/*/*/*','*/*/*/*/*/*']},
+    package_data={'': ['*', '*/*', '*/*/*',
+                       '*/*/*/*', '*/*/*/*/*', '*/*/*/*/*/*']},
     # include_package_data=True,
     install_requires=[
         "pybind11",
@@ -51,7 +56,7 @@ setuptools.setup(
         "pillow",
         "astunparse",
     ],
- )
+)
 
 # upload to pip:
 # rm -rf dist && python3.7 ./setup.py sdist && python3.7 -m twine upload dist/*
